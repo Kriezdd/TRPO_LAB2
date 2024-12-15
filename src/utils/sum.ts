1a5da5de8052c36473c3b39c86f968ef8252e0f0
kriezdd <blagushin_nikita@mail.ru>
2024-12-15 18:25:45 +0300

    added auto documentation

--- src/utils/sum.ts
+++ src/utils/sum.ts
@@ -1,3 +1,9 @@
+/**
+ * Считает сумму двух чисел
+ * @param {number} a - Первое число
+ * @param {number} b - Второе число
+ * @returns {number} Сумма двух чисел
+ */
 export function sum (a: number, b: number) {
   return a + b;
 }
