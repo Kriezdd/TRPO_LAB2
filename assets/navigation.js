f9302e45eb081bcc4bc88f779c0d0865e801b4e2
Kriezdd <Kriezdd@users.noreply.github.com>
2024-12-15 15:48:45 +0000

    deploy: 7e22be69cce04f956dce565ddb22f320938f1a05

---
+++ assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "eJyNzkEKwjAQheG7vHWwIOIiu56jFAlNSkKTNJgZEKR3F6s0rdm4neF9/N0TZB4EiTYlCCRFFhJh1uxNbtqUTpaCh8DkooY8CwzWeX03EbLbxtqMij0VYOQ4kJvjh/i+j9T1svSL2IigXKwD3tefgt2GyfncZA71cH3dMoe/+g9EaS9IBa31/QuqkGvW"
